bba099b164798a91fa27e256298e844b57a50a1c
pitpimon <pitpimon.c@itm.kmutnb.ac.th>
2024-02-13 12:45:22 +0700

    .

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const axios = require('axios')
 const app = express()
 var bodyParser = require('body-parser')
 
-const base_url = "http://localhost:3000"
+//const base_url = "http://localhost:3000"
+const base_url = "http://node58833-env-pitpimon.proen.app.ruk-com.cloud/"
 
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
